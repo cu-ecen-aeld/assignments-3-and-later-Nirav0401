8be47711fbf3f9a51ae57b8a53047c77971b752d
Lucas R <lucasmrosalino@gmail.com>
2024-07-17 11:04:42 +0300

    Make finder.sh compatible with /bin/sh

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 : '
 Accepts the following runtime arguments: the first argument is a path to a directory on the filesystem, referred to below as filesdir; the second argument is a text string which will be searched within these files, referred to below as searchstr
@@ -10,7 +10,7 @@ Accepts the following runtime arguments: the first argument is a path to a direc
 - Prints a message "The number of files are X and the number of matching lines are Y" where X is the number of files in the directory and all subdirectories and Y is the number of matching lines found in respective files, where a matching line refers to a line which contains searchstr (and may also contain additional content).
 '
 
-if ! [[ $# -eq 2 ]]; then
+if ! [ $# -eq 2 ]; then
     echo "usage: $0 path text"
     exit 1
 fi
@@ -18,13 +18,13 @@ fi
 FILESDIR=$1
 SEARCHSTR=$2
 
-if ! [[ -d ${FILESDIR} ]]; then
+if ! [ -d "${FILESDIR}" ]; then
     echo "Not a directory: ${FILESDIR}"
     exit 1
 fi
 
-FILECNT=$(find ${FILESDIR} -type f | wc -l)
-MATCHCNT=$(grep --count --recursive ${SEARCHSTR} ${FILESDIR} | awk -F: '{s+=$2} END {print s}')
+FILECNT=$(find "${FILESDIR}" -type f | wc -l)
+MATCHCNT=$(grep --count --recursive "${SEARCHSTR}" "${FILESDIR}" | awk -F: '{s+=$2} END {print s}')
 
 echo "The number of files are ${FILECNT} and the number of matching lines are ${MATCHCNT}."
 
